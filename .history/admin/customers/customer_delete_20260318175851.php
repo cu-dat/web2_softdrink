34f25954e6be8bda56e2d36e4dcb863124380e21
Gia Hưng <ngogiahung.hcm@gmail.com>
2026-04-02 20:22:30 +0700

    fizz

--- .history/admin/customers/customer_delete_20260318175851.php
+++
@@ -1,9 +0,0 @@
-<?php
-include "../config/database.php";
-
-$id = $_GET['id'];
-
-$conn->query("DELETE FROM customers WHERE id = $id");
-
-header("Location: list.php");
-?>
--- .history/admin/hash_20260331171023.php
+++
@@ -1,3 +0,0 @@
-var_dump($user['password']);
-var_dump(password_verify('admin123', $user['password']));
-exit;
