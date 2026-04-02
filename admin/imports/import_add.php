a07091ce7f0249d8f35a38d767176bd014f2403a
Gia Hưng <ngogiahung.hcm@gmail.com>
2026-04-02 21:49:04 +0700

    full code

--- admin/imports/import_add.php
+++
@@ -1,8 +0,0 @@
-<?php
-require_once '../config/database.php';
-
-$conn->query("INSERT INTO imports (status) VALUES ('draft')");
-$id = $conn->insert_id;
-
-header("Location: import_add_item.php?id=$id");
-exit();
